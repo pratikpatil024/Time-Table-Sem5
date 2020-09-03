fdd03d85e3c55957bc4be40246854681f97123ea
pratikpatil024 <pratikspatil024@gmail.com>
2020-09-03 12:31:27 +0530

    Monday Done

--- timeTable.py
+++ timeTable.py
@@ -9,14 +9,25 @@ time = datetime.datetime.now()
 time = time.strftime("%H:%M:%S")
 print(time)
 
+
 # Monday
 if day == 'Monday':
 
+  # Computer Networks
+  if time >= '08:25:00' and time <= '09:50:00':
+    print('Computer Networks')
+    webbrowser.open('https://iitbhilai.webex.com/meet/b203', new=2)
+
+  # Contemporary Indian Cinema: Beyond Bollywood
   if time >= '11:25:00' and time <= '12:50:00':
+    print('Contemporary Indian Cinema: Beyond Bollywood')
     webbrowser.open('https://iitbhilai.webex.com/meet/b202', new=2)
 
-  if time >= '11:25:00' and time <= '12:50:00':
-    webbrowser.open('', new=2)
+  # Data Analytics and Visualization
+  if time >= '14:25:00' and time <= '15:50:00':
+    print('Data Analytics and Visualization')
+    webbrowser.open('https://iitbhilai.webex.com/meet/b203', new=2)
+
 
 # Tuesday
 if day == 'Tuesday':
